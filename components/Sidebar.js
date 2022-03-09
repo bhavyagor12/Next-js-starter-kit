39696a840583a87d597264896e89dc09bfd6546b
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-03-09 15:27:20 +0530

    sidebar fixs

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,30 +1,39 @@
 import React from 'react'
-import {HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon} from "@heroicons/react/outline";
 
+import {AiOutlineHome} from 'react-icons/ai'
+import {CgProfile} from 'react-icons/cg'
+import {BsNewspaper} from 'react-icons/bs'
+import {VscFileSubmodule, VscSearch} from 'react-icons/vsc'
+import {BsChatSquareDots} from 'react-icons/bs'
 export default function Sidebar() {
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
       <div className="space-y-4">
-            <button className="flex items-center space-x-2 hover:text-white">
-              <User-circleIcon className="h-5 w-5" />
+            <button className="flex items-center space-x-2 hover:text-white"> 
+              <CgProfile className="h-5 w-5"  />
               <p>Profile</p>
           </button>
           <button className="flex items-center space-x-2 hover:text-white">
-              <HomeIcon className="h-5 w-5" />
+              <AiOutlineHome  className="h-5 w-5" />
               <p>Home</p>
           </button>
           <button className="flex items-center space-x-2 hover:text-white">
-              <HomeIcon className="h-5 w-5" />
-              <p>My Projects</p>
+              <VscFileSubmodule className="h-5 w-5" />
+              <p> Post a Projects</p>
           </button>
           <button className="flex items-center space-x-2 hover:text-white">
-              <HomeIcon className="h-5 w-5" />
-              <p>Home</p>
+              <VscSearch className="h-5 w-5" />
+              <p>Find Projects</p>
           </button>
           <button className="flex items-center space-x-2 hover:text-white">
-              <NewspaperIcon className="h-5 w-5" />
+              <BsNewspaper  className="h-5 w-5" />
               <p>News</p>
           </button>
+          <button className="flex items-center space-x-2 hover:text-white">
+              <BsChatSquareDots  className="h-5 w-5" />
+              <p>Chat</p>
+          </button>
+        
           <hr className="border-t-[0.1px] border-gray-900"/>
       </div>
     </div>
---
+++ pages/homePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+function homePage() {
+  return (
+   <>
+   <Sidebar/>
+   </>
+  )
+}
+
+export default homePage
