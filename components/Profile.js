b44ce0d93a0591d59d88290b9908c575e1daf7ef
DevrajMishra <devraj.dm007@gmail.com>
2022-03-09 14:43:57 +0530

    firse profile

---
+++ components/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+const Profile = () => {
+  return (
+    <div className='max-w-6xl mx-5 p-10 xl:mx-auto'>
+    <div className='grid grid-cols-4 gap 4'>
+    <div className='avatar'>
+        <div className='rounded-full w-36 h-36'>
+        <Image src="https://api.lorem.space/image/face?hash=92310">
+            </div>
+
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Profile
---
+++ pages/profile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import Particle from '../components/Particles'
+import Logo from '../assets/logo-removebg-preview.png'
+import { profile } from 'console'
+import Profile from '../components/Profile'
+
+const profile = () => {
+  return (
+    <main className=" flex flex-col min-h-screen bg-[#032A5D]">
+  <Head>
+    <title>Profile page</title>
+  </Head>
+  <div className="">
+  <Particle/>
+  </div>
+  <div className="profile pt-20">
+  <Profile/>
+  </div>
+  </main>
+  )
+}
+
+export default profile
